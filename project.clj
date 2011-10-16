d14c21951b53f27a683082514440d3a6fce6468a
Joseph Abrahamson <abrahamson.j@gmail.com>
2011-10-16 00:39:31 -0400

    * project.clj: Fixed some obvious changes and added beanstalk

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
-(defproject clj "1.0.0"
+(defproject tappan "0.0.1"
   :repositories {"releases" "repo.maven.org/maven2"}
-  :dev-dependencies [[lein-javac "1.2.1-SNAPSHOT"]
-                     [clojurecheck "2.0.2"]]
-  :main clj.BasicWorker
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.cli "0.1.0"]
                  [com.googlecode.efficient-java-matrix-library/ejml "0.17"]
+                 [org.clojars.sunng/beanstalk "1.0.6"]
                  [congomongo "0.1.5-SNAPSHOT"]]
   :target-dir "target/")
