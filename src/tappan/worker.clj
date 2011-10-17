9d64b276356866df213533675f16aef7c841d5af
Joseph Abrahamson <abrahamson.j@gmail.com>
2011-10-16 23:36:53 -0400

    * src/tappan/worker.clj: Library for building workers.
    Includes things like mongo connectivity and commandline parsing

---
+++ src/tappan/worker.clj
@@ -0,0 +1,18 @@
+(ns tappan.worker
+  (:use clojure.tools.cli)
+  (:require [somnium.congomongo :as cm]))
+
+(defmacro mongo-group []
+  `(group "--mongo"
+          (optional ["-h" "--host" "Mongo host"
+                     :default "127.0.0.1"])
+          (optional ["-p" "--port" "Mongo server port"
+                     :default "13241"]
+                    #(Integer/parseInt %))
+          (optional ["-db" "--database" "Database name"
+                     :default "gold"])))
+
+(defmacro with-invoked-mongo [inv & body]
+  `(let [mongo# (:mongo ~inv)]
+     (cm/with-mongo (cm/make-connection (:database mongo#) mongo#)
+       ~@body)))
