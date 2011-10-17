48c7333acf18085318c4d447bfe852da379de7f4
Joseph Abrahamson <abrahamson.j@gmail.com>
2011-10-16 23:22:27 -0400

    * src/tappan/util.clj: take-while2: take-while over the last *two* elements.

--- src/tappan/util.clj
+++ src/tappan/util.clj
@@ -1,6 +1,20 @@
 ;;; Some obvious utility functions
 (ns tappan.util)
 
+
+;;; Convergence take
+(defn take-while2
+  "Like take-while but pred is applied to x(i-1) and x(i)."
+  [pred coll]
+  (lazy-seq
+   (when-let [s (seq coll)]
+     (let [[x1 x2 & rest] s]
+       (if x2
+         (if (pred x1 x2)
+           (cons x1 (take-while2 pred (cons x2 rest)))
+           (list x1 x2))
+         (list x1))))))
+
 ;;; Function utilities
 ;;;
 (defn flip [f] (fn [a b] (f b a)))
