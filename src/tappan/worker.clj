1b22b9cc12387eb0346e6a7c0d926c7624cd1deb
Joseph Abrahamson <abrahamson.j@gmail.com>
2011-11-09 15:03:58 -0500

    kmeans_purity worker refactorization along with die-roboters integration.

--- src/tappan/worker.clj
+++ src/tappan/worker.clj
@@ -1,19 +1,42 @@
 (ns tappan.worker
   (:use clojure.tools.cli)
-  (:require [somnium.congomongo :as cm]))
+  (:require [somnium.congomongo :as cm]
+            [die.roboter :as roboter]))
 
-(defmacro mongo-group []
-  `(group "--mongo"
-          (optional ["-h" "--host" "Mongo host"
-                     :default "127.0.0.1"])
-          (optional ["-p" "--port" "Mongo server port"
-                     :default "13241"]
-                    #(Integer/parseInt %))
-          (optional ["-db" "--database" "Database name"
-                     :default "gold"]
-                    keyword)))
+(defn mongo-group []
+  (group "--mongo"
+         (optional ["-h" "--host" "Mongo host"
+                    :default "127.0.0.1"])
+         (optional ["-p" "--port" "Mongo server port"
+                    :default "13241"]
+                   #(Integer/parseInt %))
+         (optional ["-db" "--database" "Database name"
+                    :default "gold"]
+                   keyword)))
+(defn roboter-group []
+  (group "--roboter"
+         (optional ["-h" "--host" "AMQP host"])
+         (optional ["-p" "--port" "AMQP port"
+                    :default "11300"]
+                   #(Integer/parseInt %))
+         (optional ["-u" "--username" "AMQP user"
+                    :default "guest"])
+         (optional ["-x" "--password" "AMQP password"
+                    :default "guest"])))
 
 (defmacro with-invoked-mongo [inv & body]
   `(let [mongo# (:mongo ~inv)]
      (cm/with-mongo (cm/make-connection (:database mongo#) mongo#)
        ~@body)))
+
+(defn do-the-roboter
+  "Turn into a Roboter using the invocation configuration"
+  [inv]
+  (roboter/work
+   (select-keys (:roboter inv)
+                (for [[k v] (:roboter inv) :when v] k))))
+
+(defn die-in [t]
+  (future
+    (Thread/sleep (* t 1000))
+    (System/exit 0)))
--- src/tappan/workers/kmeans_purity.clj
+++ src/tappan/workers/kmeans_purity.clj
@@ -5,10 +5,9 @@
         [cljfastdtw core])
   (:require [somnium.congomongo :as cm]
             [tappan.matrix :as m]
-            [tappan.data.swbd :as swbd])
+            [tappan.data.swbd :as swbd]
+            [die.roboter :as roboter])
   (:gen-class))
-
-;;; Use dist30p for useful knot coverage. This is 888 entries.
 
 (defn get-data
   "Gets SWID START STOP for utterances corresponding to a particular word"
@@ -29,170 +28,97 @@
         totals (map count clusters)]
     (float (/ (reduce + cts) (reduce + totals)))))
 
-(defn eigenspace-of [words n]
-  (let [ee (apply concat
-                  (for [w1 words w2 words]
-                    (cm/fetch :edges :where {:words [w1 w2]})))
-        lines (set (mapcat :lines ee))
-        nlines (count lines)
-        line->id (apply merge
-                        (map-indexed (fn [i z] {z i})
-                                     lines))
-        id->line (map-invert line->id)]
-    (when (not (empty? lines))
-      (let [W (m/from-sparse nlines nlines
-                             (mapcat (fn [e]
-                                       (let [[l1 l2] (map line->id (:lines e))
-                                             dist (:dtw e)
-                                             w (Math/exp (- (/ (* dist dist) (* 2 0.1 0.1))))]
-                                         [[l1 l2 w]
-                                          [l2 l1 w]
-                                          [l1 l1 (/ w 2)]
-                                          [l2 l2 (/ w 2)]]))
-                                     ee))
-            line->label (apply merge
-                               (map (fn [line]
-                                      {line
-                                       (:word (cm/fetch-one :utterances
-                                                            :where {:line line}))})
-                                    lines))
-            labels (map (fn [i] (-> i id->line line->label)) (range nlines))
-            Dhalf (m/diag (map #(/ (Math/sqrt (reduce + (m/gets* W % :_)))) (range nlines)))
-            lap (m/prod Dhalf W Dhalf)
-            {:keys [vals vecs]} (m/eig lap)
-            espace0 (apply m/then (take n vecs))
-            espace (apply m/above (map m/normalize (m/rows espace0)))]
-        [labels espace lap]))))
+(defn data-and-key
+  "Get all the SWBD entries for a word pairing along with a key to
+  differentiate their identities"
+  [pair]
+  (let [[u1 u2] (map get-data (:words pair))
+        data (concat u1 u2)
+        n1 (count u1)
+        n2 (count u2)
+        n  (count data)
+        wordkey (apply merge
+                       (concat
+                        (map (fn [i] {i 0}) (range n1))
+                        (map (fn [i] {i 1}) (range n1 n))))]
+    [data wordkey]))
 
-(defn compute-purity [n]
-  (let [pairs (cm/fetch :worddist
-                        :where {:kmeans_purity 0
-                                :dist20p {:$exists true}
-                                :nedges {:$gte 3}
-                                :identical false}
-                        :limit n)]
-    (when pairs ;; nil out otherwise
-      (doseq [pair pairs]
-        (println (:words pair))
-        (let [[u1 u2] (map get-data (:words pair))
-              data (concat u1 u2)
-              n1 (count u1)
-              n2 (count u2)
-              n (+ n1 n2)
-              wordkey (apply merge
-                             (concat
-                              (map (fn [i] {i 0}) (range n1))
-                              (map (fn [i] {i 1}) (range n1 n))))
-              ;; Compute the kernel k-means using a exponential RBF
-              ;; kernel on the distance
-              variance (* 20 20)
-              kmeans-part (kmeans:ks
-                           #(let [{:keys [swid start stop]} (nth data %1)
-                                  o1 (swbd/read-post swid start stop)
-                                  {:keys [swid start stop]} (nth data %2)
-                                  o2 (swbd/read-post swid start stop)
-                                  dist (m-dtw-dist o1 o2 10)
-                                  sim (Math/exp (- (/ (Math/pow dist 2)
-                                                      (* 2 variance))))]
-                              ;; (println [sim dist])
-                              sim) n 2)
-              pty (purity (map (fn [cluster] (map wordkey cluster)) kmeans-part))]
-          (println "Purity between" (:words pair) "is:" pty)
-          (cm/update! :worddist pair {:$set {:kmeans_purity pty}})))
-      (count pairs))))
+(defn espace-of
+  ([affinity] (let [[n d] (m/size affinity)] (espace-of affinity n)))
+  ([affinity n]
+     (let [[n d] (m/size affinity)
+           root-d (m/diag
+                   (for [i (range n)]
+                     (/ (Math/sqrt
+                         (reduce
+                          + (m/gets* affinity i :_))))))
+           lap (m/prod root-d affinity root-d)
+           {:keys [_ vecs]} (m/eig lap)]
+       (apply m/above (map m/normalize (m/rows (apply m/then (take n vecs))))))))
 
-(defn compute-purity-espace [n]
-  (let [pairs (cm/fetch :worddist
-                        :where {:espace_purity 0
-                                :dist20p {:$exists true}
-                                :nedges {:$gte 3}
-                                :identical false}
-                        :limit n)]
-    (when pairs ;; nil out otherwise
-      (doseq [pair pairs]
-        (println (:words pair))
-        (let [[u1 u2] (map get-data (:words pair))
-              data (concat u1 u2)
-              n1 (count u1)
-              n2 (count u2)
-              n (+ n1 n2)
-              wordkey (apply merge
-                             (concat
-                              (map (fn [i] {i 0}) (range n1))
-                              (map (fn [i] {i 1}) (range n1 n))))
-              ;; Compute the kernel k-means using a exponential RBF
-              ;; kernel on the distance
-              variance (* 20 20)
-              W (m/from-sparse
-                 n n
-                 (apply concat
-                        (for [i (range n) j (range i n)]
-                          (let [{:keys [swid start stop]} (nth data i)
-                                o1 (swbd/read-post swid start stop)
-                                {:keys [swid start stop]} (nth data j)
-                                o2 (swbd/read-post swid start stop)
-                                dist (m-dtw-dist o1 o2 10)
-                                sim (Math/exp (- (/ (Math/pow dist 2)
-                                                    (* 2 variance))))]
-                            [[i j sim] [j i sim]]))))
-              Dhalf (m/diag (map #(/ (Math/sqrt (reduce + (m/gets* W % :_)))) (range n)))
-              lap (m/prod Dhalf W Dhalf)
-              {:keys [vals vecs]} (m/eig lap)
-              espace0 (apply m/then (take n vecs))
-              espace (apply m/above (map m/normalize (m/rows espace0)))              
-              kmeans-part (kmeans:ks #(m/dot (m/nth-row espace %1) (m/nth-row espace %2)) n 2)
-              pty (purity (map (fn [cluster] (map wordkey cluster)) kmeans-part))]
-          (println "Purity between" (:words pair) "is:" pty)
-          (cm/update! :worddist pair {:$set {:espace_purity pty}})))
-      (count pairs))))
+(defn instance-distance-matrix [data]
+  (let [n (count data)]
+    (m/from-sparse
+     n n
+     (apply concat
+            (for [i (range n) j (range i n)]
+              (let [{:keys [swid start stop]} (nth data i)
+                    o1 (swbd/read-post swid start stop)
+                    {:keys [swid start stop]} (nth data j)
+                    o2 (swbd/read-post swid start stop)
+                    dist (m-dtw-dist o1 o2 10)]
+                [[i j dist] [j i dist]]))))))
 
-;; (defn compute-purity-espace [n]
-;;   (let [pairs (cm/fetch :worddist
-;;                         :where {:espace_purity 0
-;;                                 :dist20p {:$exists true}
-;;                                 :nedges {:$gte 3}
-;;                                 :identical false}
-;;                         :limit n)]
-;;     (when pairs ;; nil out otherwise
-;;       (doseq [pair pairs]
-;;         (let [[labels espace] (eigenspace-of (:words pair) 10000)
-;;               z labels
-;;               kmeans-part (kmeans:ks #(m/dot (m/nth-row espace %1) (m/nth-row espace %2))
-;;                                      (count labels) 2)
-;;               pty (purity (map (fn [cluster] (map (partial nth labels) cluster)) kmeans-part))]
-;;           (println "Purity between" (:words pair) "is:" pty)
-;;           (cm/update! :worddist pair {:$set {:espace_purity pty}})))
-;;       (count pairs))))
+(defn compute-purity*
+  "Find the purity between instances of a particular word pair using
+  kernel-kmeans with a gram matrix computed as (dist-to-aff
+  dtw-distances)."
+  [pair dist-to-aff]
+  (let [[data wordkey] (data-and-key pair)
+        n (count data)
+        aff (dist-to-aff (instance-distance-matrix data))
+        kmeans-part (kmeans:ks (partial m/get aff) n 2)
+        pty (purity (map (fn [cluster] (map wordkey cluster)) kmeans-part))]
+    (println "Purity between" (:words pair) "is:" pty)))
 
+(defn compute-purity-kmeans
+  ([pair] (compute-purity-kmeans
+           pair #(Math/exp (- (/ (Math/pow % 2)
+                                 (* 2 20 20))))))
+  ([pair simfn]
+     (compute-purity*
+      pair
+      (partial m/map simfn))))
 
-;;; I'll leave this overnight...
-;; (def fut (future (compute-purity 130)))
-
-(defn -main [& args]
-  (let [inv (cli args (mongo-group))]
-    (with-invoked-mongo inv
-      (loop []
-        (if-let [pairs (compute-purity 10)]
-          (recur))))))
+(defn compute-purity-espace
+  ([pair] (compute-purity-espace
+           pair #(Math/exp (- (/ (Math/pow % 2)
+                                 (* 2 20 20))))))
+  ([pair simfn]
+     (compute-purity*
+      pair
+      (fn [D]
+        (let [espace (espace-of
+                      (m/map simfn D))]
+          (m/prod espace (m/t espace)))))))
 
 ;; Levenshtein distances
-;;; 
-;; (let [pairs (cm/fetch :worddist
-;;                       :where {:kmeans_purity {:$exists true}}
-;;                       :only [:words])]
-;;   (doseq [pair pairs]
-;;     (let [words (:words pair)
-;;           w1 (cm/fetch-one :words :where {:word (get words 0)})
-;;           w2 (cm/fetch-one :words :where {:word (get words 1)})
-;;           ps1 (:phonetics w1)
-;;           ps2 (:phonetics w2)
-;;           editdists (for [p1 ps1 p2 ps2] (/ (levenshtein p1 p2) (+ (count p1) (count p2))))
-;;           meanedit (if (empty? editdists)
-;;                      nil
-;;                      (/ (reduce + editdists) (count editdists)))]
-;;       (println "D[" ps1 ps2 "] : " meanedit)
-;;       (cm/update! :worddist pair {:$set {:levenshtein editdists
-;;                                          :meanlevenshtein meanedit}}))))
+;;;
+
+(defn set-levenshtein [pairid]
+  (let [pair (cm/fetch-one :worddist :where {:_id (cm/object-id pairid)})
+        words (:words pair)
+        w1 (cm/fetch-one :words :where {:word (get words 0)})
+        w2 (cm/fetch-one :words :where {:word (get words 1)})
+        ps1 (:phonetics w1)
+        ps2 (:phonetics w2)
+        editdists (for [p1 ps1 p2 ps2] (/ (levenshtein p1 p2) (+ (count p1) (count p2))))
+        meanedit (if (empty? editdists)
+                   nil
+                   (/ (reduce + editdists) (count editdists)))]
+    (println "D[" ps1 ps2 "] : " meanedit)
+    (cm/update! :worddist pair {:$set {:levenshtein editdists
+                                       :meanlevenshtein meanedit}})))
 
 (defn output-dists []
   (spit "data/dists.csv"
@@ -208,3 +134,17 @@
                               :where {:espace_purity {:$gt 0}
                                       :kmeans_purity {:$gt 0}
                                       :identical false})))))
+
+;;; The worker body
+(defn -main [& args]
+  (let [inv (cli args
+                 (mongo-group)
+                 (roboter-group))]
+    (die-in (* 5 60 60)) ;; 60 minute run time
+    (with-invoked-mongo inv
+      (binding [roboter/*exception-handler*
+                (fn [e msg]
+                  (println "Oh, we got trouble!" (.getMessage e))
+                  (System/exit 1))]
+        (println "Robot: ONLINE!")
+        (do-the-roboter inv)))))
