158f6ee9f68aa21a7f013c547715d11a5187541a
Joseph Abrahamson <abrahamson.j@gmail.com>
2011-11-09 15:03:15 -0500

    Colorized clj-stacktrace

--- project.clj
+++ project.clj
@@ -1,12 +1,15 @@
-(defproject tappan/tappan "0.0.1"
+(defproject tappan/tappan "0.0.1-SNAPSHOT"
   :description "The core of Tappan, also its growing warm ember."
   :warn-on-reflection true
   :repositories {"releases" "repo.maven.org/maven2"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [tappan/q "0.0.1-SNAPSHOT"]
+                 [org.clojure/tools.cli "0.1.0"]
+                 [die-roboter "1.0.0-SNAPSHOT"]
                  [tappan/matrix "0.0.1-SNAPSHOT"]
-                 [org.clojars.pallix/analemma "1.0.0-SNAPSHOT"]
+                 [tappan/q "0.0.1-SNAPSHOT"]
+                 [tappan/util "0.0.1-SNAPSHOT"]
                  [cljfastdtw "1.0.4-SNAPSHOT"]
-                 [org.clojars.sunng/beanstalk "1.0.6"]
                  [congomongo "0.1.7"]]
-  :target-dir "target/")
+  :target-dir "target/"
+  :repl-options [:init nil :caught clj-stacktrace.repl/pst+]
+  :clj-stacktrace {:test-color true})
