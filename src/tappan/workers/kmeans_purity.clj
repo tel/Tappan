0803cbd129dab10d4af379689a8a4a09e7c6f1e4
Joseph Abrahamson <abrahamson.j@gmail.com>
2011-11-18 13:51:34 -0500

    Changes in kmeans_purity worker

--- src/tappan/workers/kmeans_purity.clj
+++ src/tappan/workers/kmeans_purity.clj
@@ -79,7 +79,7 @@
         aff (dist-to-aff (instance-distance-matrix data))
         kmeans-part (kmeans:ks (partial m/get aff) n 2)
         pty (purity (map (fn [cluster] (map wordkey cluster)) kmeans-part))]
-    (println "Purity between" (:words pair) "is:" pty)))
+    pty))
 
 (defn compute-purity-kmeans
   ([pair] (compute-purity-kmeans
@@ -102,8 +102,45 @@
                       (m/map simfn D))]
           (m/prod espace (m/t espace)))))))
 
+(defn- order [vec]
+  (let [keyed (map-indexed list vec)]
+    (map first (sort-by second keyed))))
+
+(defn- is-sparse-vec
+  [[x & _]] (not (sequential? x)))
+
+(defn- flatten-to-sparse* [vs]
+  (loop [vs vs build nil]
+    (let [[v & vs] vs]
+     (if (nil? v)
+       (vec build)
+       (if (is-sparse-vec v)
+         (recur vs (cons v build))
+         (recur vs (concat (flatten-to-sparse* v) build)))))))
+
+(defn- flatten-to-sparse [vs]
+  (reverse (flatten-to-sparse* vs)))
+
+(defn dist-to-knn
+  "Generate a kNN linkage affinity from a distance matrix. Not
+  particularly speedy."
+  [k D]
+  (let [n (first (m/size D))]
+    (m/from-sparse n n
+     (flatten-to-sparse
+            (for [i (range n)]
+              (let [indices (take (inc k) (order (m/gets* D i :_)))]
+                (map (fn [j] [[i j 1] [j i 1]]) indices)))))))
+
+(defn set-dist-attr [id fn key]
+  (let [pair (cm/fetch-one :worddist
+                           :where {:_id (cm/object-id id)}
+                           :only [:words])]
+    (cm/update! :worddist pair {:$set {key (fn pair)}})))
+
 ;; Levenshtein distances
 ;;;
+
 
 (defn set-levenshtein [pairid]
   (let [pair (cm/fetch-one :worddist :where {:_id (cm/object-id pairid)})
@@ -120,27 +157,56 @@
     (cm/update! :worddist pair {:$set {:levenshtein editdists
                                        :meanlevenshtein meanedit}})))
 
+(defn write-stats [f & keys]
+  (let [k2s  #(subs (str %) 1)
+        blankify #(or % " ")
+        keys0 keys
+        basic-keys [:words :nedges :npairs :meanlevenshtein]
+        keys (concat basic-keys keys0)
+        pairs (cm/fetch :worddist
+                        :where (apply merge
+                                      (map (fn [k] {k {:$exists true}})
+                                           ;; We accept nils, but not
+                                           ;; in the first key
+                                           ;; (concat (take 1 keys0) basic-keys)
+                                           keys))
+                        :only keys)
+        _ (println (count pairs))
+        fmt (apply str "%s, %s, %s, %s, "
+                   (interpose ", " (repeat (count keys0) "%s")))
+        comps (map #(map (comp blankify
+                               (partial get %))
+                         keys)
+                   pairs)]
+    (spit
+     f (with-out-str
+         (println (apply format fmt (map k2s keys)))
+         (doseq [pair pairs]
+           (let [[[w1 w2] & rest] (map (partial get pair) keys)]
+             (println (apply format fmt (str w1 " " w2) rest))))))))
+
 (defn output-dists []
-  (spit "data/dists.csv"
-        (apply str
-               "words, nedges, mindist, dist20p, dist30p, maxdist, meanlevenshtein, espace_purity, espace_purity2, kmeans_purity, meandist\n"
-               (map (fn [{:keys [words nedges mindist dist20p dist30p maxdist
-                                 meanlevenshtein espace_purity espace_purity2 kmeans_purity meandist]}]
-                      (format "%s %s, %d, %s, %s, %s, %s, %s, %s, %s, %s, %s\n"
-                              (first words) (second words)
-                              nedges (or mindist " ") (or dist20p " ") (or dist30p " ") (or maxdist " ")
-                              (or meanlevenshtein " ") (or espace_purity " ") (or espace_purity2 " ") (or kmeans_purity " ") (or meandist " ")))
-                    (cm/fetch :worddist
-                              :where {:espace_purity {:$gt 0}
-                                      :kmeans_purity {:$gt 0}
-                                      :identical false})))))
+  (write-stats "data/dists.csv"
+               :espace_purity
+               :espace_purity_knn2
+               :espace_purity_knn8
+               :espace_purity_sigma10
+               :espace_purity_sigma5
+               :kmeans_purity
+               :kmeans_purity_knn2
+               :kmeans_purity_knn8
+               :kmeans_purity_sigma10
+               :kmeans_purity_sigma5))
 
 ;;; The worker body
 (defn -main [& args]
   (let [inv (cli args
+                 (optional ["-t" "--ttl" "Time to live in minutes."
+                            :default (str (* 5 60))]
+                           #(Float/parseFloat %))
                  (mongo-group)
                  (roboter-group))]
-    (die-in (* 5 60 60)) ;; 60 minute run time
+    (die-in (* (:ttl inv) 60)) ;; 60 minute run time
     (with-invoked-mongo inv
       (binding [roboter/*exception-handler*
                 (fn [e msg]
@@ -148,3 +214,46 @@
                   (System/exit 1))]
         (println "Robot: ONLINE!")
         (do-the-roboter inv)))))
+
+(def task-map
+  {:espace_purity (fn [pair] `(compute-purity-espace ~pair))
+   :kmeans_purity (fn [pair] `(compute-purity-kmeans ~pair))
+   ;; Using only two eigenvectors
+   :espace_purity2 (fn [pair]
+                     `(compute-purity* ~pair
+                        (fn [D#]
+                          (let [espace# (espace-of
+                                         (m/map simfn D) 2)]
+                            (m/prod espace# (m/t espace#))))))
+   :espace_purity_knn2 (fn [pair]
+                         `(compute-purity* ~pair
+                            (fn [D#]
+                              (let [knn# (dist-to-knn 2 D#)
+                                    espace# (espace-of
+                                             (m/map simfn D))]
+                                (m/prod espace# (m/t espace#))))))
+   :espace_purity_knn8 (fn [pair]
+                         `(compute-purity* ~pair
+                            (fn [D#]
+                              (let [knn# (dist-to-knn 8 D#)
+                                    espace# (espace-of
+                                             (m/map simfn D))]
+                                (m/prod espace# (m/t espace#))))))
+   :espace_purity_sigma10 (fn [pair] `(compute-purity-espace ~pair
+                                        #(Math/exp (- (/ (Math/pow % 2)
+                                                         (* 2 10 10))))))
+   :espace_purity_sigma5 (fn [pair] `(compute-purity-espace ~pair
+                                       #(Math/exp (- (/ (Math/pow % 2)
+                                                        (* 2 5 5))))))
+   :kmeans_purity_knn2 (fn [pair]
+                         `(compute-purity* ~pair
+                            (partial dist-to-knn 2)))
+   :kmeans_purity_knn8 (fn [pair]
+                         `(compute-purity* ~pair
+                            (partial dist-to-knn 8)))
+   :kmeans_purity_sigma10 (fn [pair] `(compute-purity-kmeans ~pair
+                                        #(Math/exp (- (/ (Math/pow % 2)
+                                                         (* 2 10 10))))))
+   :kmeans_purity_sigma5 (fn [pair] `(compute-purity-kmeans ~pair
+                                       #(Math/exp (- (/ (Math/pow % 2)
+                                                        (* 2 5 5))))))})
